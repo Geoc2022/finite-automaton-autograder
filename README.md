892bb2090d67acd423c46c3937832da83bbe0569
Geoc2022 <geoc2022@gmail.com>
2025-10-11 13:40:00 -0400

    Init prototype

---
+++ README.md
@@ -0,0 +1,42 @@
+# FA Grader
+
+Prototype of a tool for 1010 students to use for creating and testing their FA.
+
+Currently there is only support for DFAs.
+
+## Rendering a Python Object DFA
+
+1. Run `python dfa.py` to create a DFA object as an example in the main function
+2. Run a local server using `python -m http.server` in the terminal in this directory
+3. Open your browser and go to `http://localhost:8000/dfa_render.html`
+
+This should be using the dfa.json file in the directory created by `dfa.py`.
+You can edit the JSON file to get a different result or create your own DFA object in python and save it as a JSON file
+
+![Render](./img/render.png)
+
+## Creating a DFA and Converting it to a Python Object
+
+1. Run a local server using `python -m http.server` in the terminal in this directory
+2. Open your browser and go to `http://localhost:8000/dfa_create.html`
+3. Create your DFA using the instructions on the top of the page
+4. Click the "Export DFA as JSON" button to download the DFA as a JSON file
+5. Run `python dfa.py` and modify the main function to load your JSON file instead of dfa.json
+
+![Create](./img/create.png)
+
+## TODO
+
+- Add everything (e.g. group names) in `dfa_render.html` to `dfa_create.html` s.t. `create` \supset `render`
+
+- Make `dfa_create.html` use standard notation
+
+- Add LaTeX support to renders (e.g Greek symbols)
+
+- Add a FA library to hold the reused rendering functions
+
+- Export to LaTeX Tikz Diagrams (?)
+
+## License
+
+This uses a [force graph library](https://vasturiano.github.io/force-graph/)
---
+++ dfa.py
@@ -0,0 +1,109 @@
+import json
+from pathlib import Path
+
+class DFA:
+    def __init__(self, states, alphabet, transitions, start_state, accept_states):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.start_state = start_state
+        self.accept_states = accept_states
+
+    def accepts(self, input_string):
+        current_state = self.start_state
+        for symbol in input_string:
+            if symbol not in self.alphabet:
+                return False
+            if current_state not in self.transitions or symbol not in self.transitions[current_state]:
+                return False
+            current_state = self.transitions[current_state][symbol]
+        return current_state in self.accept_states
+
+    def to_json(self, path="dfa.json"):
+        state_index = {s: i for i, s in enumerate(self.states)}
+
+        nodes = [
+            {
+                "name": s,
+                "id": state_index[s],
+                "group": (
+                    "start" if s == self.start_state else
+                    "accept" if s in self.accept_states else
+                    "normal"
+                ),
+            }
+            for s in self.states
+        ]
+
+        links = []
+        for src, transitions in self.transitions.items():
+            for symbol, dst in transitions.items():
+                links.append({
+                    "source_name": src,
+                    "target_name": dst,
+                    "source": state_index[src],
+                    "target": state_index[dst],
+                    "label": symbol
+                })
+
+        data = {"nodes": nodes, "links": links}
+
+        Path(path).write_text(json.dumps(data, indent=2))
+        print(f"Exported DFA to {path}")
+        return data
+
+    def from_json(self, path="dfa.json"):
+        with open(path, "r") as f:
+            data = json.load(f)
+
+        states = [node["name"] for node in data["nodes"]]
+
+        start_state = None
+        accept_states = set()
+        for node in data["nodes"]:
+            if node.get("group") == "start":
+                start_state = node["name"]
+            elif node.get("group") == "accept":
+                accept_states.add(node["name"])
+        if start_state is None:
+            raise ValueError("No start state defined in JSON")
+
+        transitions = {state: {} for state in states}
+        alphabet = set()
+
+        for link in data["links"]:
+            src = link["source_name"]
+            dst = link["target_name"]
+            symbol = link.get("label", "a")
+            alphabet.add(symbol)
+            if src not in transitions:
+                transitions[src] = {}
+            transitions[src][symbol] = dst
+
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.start_state = start_state
+        self.accept_states = accept_states
+
+def main():
+    dfa = DFA(
+        states={'q0', 'q1', 'q2'},
+        alphabet={'a', 'b'},
+        transitions={
+            'q0': {'a': 'q1'},
+            'q1': {'a': 'q1', 'b': 'q2', 'c': 'q1'},
+            'q2': {'a': 'q2', 'b': 'q0'}
+        },
+        start_state='q0',
+        accept_states={'q1'}
+    )
+
+    dfa.to_json()
+    dfa.from_json()
+
+    print(dfa.accepts("aba"))
+    print(dfa.accepts("a"))
+
+if __name__ == "__main__":
+    main()
